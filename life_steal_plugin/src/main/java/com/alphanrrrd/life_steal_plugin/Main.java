19a26c3022921779077bf422e30703c976823805
manamster <calvinleo16@gmail.com>
2022-05-21 02:10:33 -0500

    Switched to maven since I dont like eclipse anymore. Also gonna re-write the whole thing since I have actually learned java now ...

---
+++ life_steal_plugin/src/main/java/com/alphanrrrd/life_steal_plugin/Main.java
@@ -0,0 +1,13 @@
+package com.alphanrrrd.life_steal_plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+public class Main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+        getLogger().info("Hello, SpigotMC!");
+    }
+    @Override
+    public void onDisable() {
+        getLogger().info("See you again, SpigotMC!");
+    }
+}
---
+++ life_steal_plugin/src/test/java/com/alphanrrrd/life_steal_plugin/AppTest.java
@@ -0,0 +1,20 @@
+package com.alphanrrrd.life_steal_plugin;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
